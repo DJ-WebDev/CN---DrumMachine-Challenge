61c23fe38b2027ff6cf426eb6033685b04301187
DJ-WebDev <99653162+DJ-WebDev@users.noreply.github.com>
2022-04-17 19:48:25 +0100

    Uploaded Loop Jacker files from DJ-STUDIO

---
+++ app.js
@@ -0,0 +1,40 @@
+
+// I had to be resourceful to make this work how i wanted it to:
+
+const songs = document.querySelectorAll("audio");
+const divs = document.querySelectorAll(".container div");
+
+window.addEventListener("keydown", (e) =>{
+  let code = e.keyCode.toString();   
+    songs.forEach(song =>{
+      if(code === song.id){
+        song.play();
+        song.loop=true;
+        song.currentTime = 0;
+    } else {                
+        song.pause();   // // THIS TOOK ME AGES TO FIGURE OUT! Seems so simple now!
+        song.currentTime = 0;  //how to pause the loops when the next one plays!!
+            
+      }
+    });
+        
+    divs.forEach(div =>{  // Some animation I have played with
+        if(code === div.id){
+            div.classList.add("animate");
+            setTimeout( () => div.classList.remove("animate"), 100);
+        }
+    })
+});
+//--------------------------------------------
+
+// I wanted the letters to turn green in the animation but i could not figure it out: 
+
+const greenFun = document.getElementsByClassName("name");
+
+window.addEventListener("keydown" , (e) => {
+  let greenFun = e.keyCode.toString();
+  greenFun.style.color = "limegreen";
+});
+
+//--------------------------------------------
+
