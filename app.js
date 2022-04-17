f536e6434d462da47deb0a404881c8abfd3c3aa9
Duncan James <djduncanjames@hotmail.com>
2022-04-17 23:15:45 +0100

    Drum Machine Animation and Transition Final

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
-
 // I had to be resourceful to make this work how i wanted it to:
 
 const songs = document.querySelectorAll("audio");
 const divs = document.querySelectorAll(".container div");
 
-window.addEventListener("keydown", (e) =>{
-  let code = e.keyCode.toString();   
-    songs.forEach(song =>{
-      if(code === song.id){
-        song.play();
-        song.loop=true;
-        song.currentTime = 0;
-    } else {                
-        song.pause();   // // THIS TOOK ME AGES TO FIGURE OUT! Seems so simple now!
-        song.currentTime = 0;  //how to pause the loops when the next one plays!!
-            
-      }
-    });
-        
-    divs.forEach(div =>{  // Some animation I have played with
-        if(code === div.id){
-            div.classList.add("animate");
-            setTimeout( () => div.classList.remove("animate"), 100);
-        }
-    })
+window.addEventListener("keydown", (e) => {
+  let code = e.keyCode.toString();
+  songs.forEach(song => {
+    if (code === song.id) {
+      song.play();
+      song.loop = true;
+      song.currentTime = 0;
+    } else {
+      song.pause(); // // THIS TOOK ME AGES TO FIGURE OUT! Seems so simple now!
+      song.currentTime = 0; //how to pause the loops when the next one plays!!
+
+    }
+  });
+
+  divs.forEach(div => { // Some animation I have played with
+    if (code === div.id) {
+      div.classList.add("animate");
+      setTimeout(() => div.classList.remove("animate"), 100);
+    }
+  })
 });
 //--------------------------------------------
 
-// I wanted the letters to turn green in the animation but i could not figure it out: 
+// I wanted the letters to turn green in the animation but i could not figure it out..even a hover.. or when user presses 
+
+// Where did I go wrong ? 
 
 const greenFun = document.getElementsByClassName("name");
 
-window.addEventListener("keydown" , (e) => {
+window.addEventListener("keydown", (e) => {
   let greenFun = e.keyCode.toString();
   greenFun.style.color = "limegreen";
 });
 
 //--------------------------------------------
-
